9efe245ce225f07ec77e14cdae1b12e01b4fc6b4
David <ruedarosasdavid@gmail.com>
2025-06-01 18:38:37 +0200

    Rama Vanilla finalizada (maquetación + responsive)

---
+++ main.js
@@ -0,0 +1,22 @@
+// Mobile menu
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu-overlay');
+const mobileMenuClose = document.querySelector('.mobile-menu-close');
+
+// Open
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.add('active');
+});
+
+// Close
+mobileMenuClose.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+});
+
+// Close when click any link
+const mobileLinks = document.querySelectorAll('.mobile-menu-nav a');
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+  });
+});
